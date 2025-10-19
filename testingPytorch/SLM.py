1a2be298a8349d18c172c9ef8dbb0128a91cd67e
MarkProMaster229 <romtomgmai@gmail.com>
2025-10-19 21:38:09 +0300

    new

--- testingPytorch/SLM.py
+++ testingPytorch/SLM.py
@@ -4,11 +4,22 @@
 
 # Use a pipeline as a high-level helper
 
-from transformers import pipeline
+import torch
+from transformers import AutoTokenizer, Phi3ForCausalLM
 
-pipe = pipeline("text-generation", model="microsoft/Phi-3.5-mini-instruct", trust_remote_code=True)
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe(messages)
+model_path = "/home/chelovek/models/Phi-3.5-mini-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+model = Phi3ForCausalLM.from_pretrained(
+    model_path,
+    trust_remote_code=True,
+    device_map={"": "cpu"},        # на CPU
+    attn_implementation="eager"    # вместо flash-attention
+)
+
+inputs = tokenizer("Hello world!", return_tensors="pt")
+outputs = model.generate(**inputs, max_new_tokens=16)
+
+print(tokenizer.decode(outputs[0]))
+
 #further
