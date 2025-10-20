ddc2d110276ec117a1611232a15be9cfd48ece13
MarkProMaster229 <romtomgmai@gmail.com>
2025-10-20 16:36:29 +0300

    testing

--- testingPytorch/SLM.py
+++ testingPytorch/SLM.py
@@ -1,25 +1,40 @@
-#testing here slm model, and working mcp-tool
-
-#dowload model slm, get
-
-# Use a pipeline as a high-level helper
-
 import torch
-from transformers import AutoTokenizer, Phi3ForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
-model_path = "/home/chelovek/models/Phi-3.5-mini-instruct"
 
-tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-model = Phi3ForCausalLM.from_pretrained(
-    model_path,
-    trust_remote_code=True,
-    device_map={"": "cpu"},        # на CPU
-    attn_implementation="eager"    # вместо flash-attention
+model_dir = "/home/chelovek/PycharmProjects/PythonProject11/model/testingPytorch/Phi-3.5-mini-int4/"
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_dir)
+print("ток")
+
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype="float16"
 )
 
-inputs = tokenizer("Hello world!", return_tensors="pt")
-outputs = model.generate(**inputs, max_new_tokens=16)
 
-print(tokenizer.decode(outputs[0]))
+model = AutoModelForCausalLM.from_pretrained(
+    model_dir,
+    device_map="auto",
+    quantization_config=bnb_config
+)
+print("модель загружена ")
 
-#further
+
+messages = [{"role": "user", "content": "Who are you?"}]
+
+inputs = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device)
+print("генер ")
+outputs = model.generate(**inputs, max_new_tokens=40)
+print("последовательность  ")
+print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:]))
